83f0458a2afa2ea564eeacb042ca2be8f16fc413
vinicius-bispo1 <vini.bispo125@gmail.com>
2026-03-24 17:21:12 -0300

    Código aula: 25/03

--- src/app.js
+++ src/app.js
@@ -8,11 +8,26 @@ app.get("/", (req, res) => {
   res.send("<h1>Rede Social!</h1>");
 });
 
+// GET - usuarios
+// app.get("/usuarios", async (req, res) => {
+//   try {
+//     const resultado = await pool.query(`
+//       SELECT
+//         *
+//       FROM usuarios;
+//     `);
+//     res.json(resultado.rows);
+//   } catch (erro) {
+//     res.status(500).json({ erro: "Erro ao buscar postagens" });
+//   }
+// });
+
 // GET POSTS
 app.get("/posts", async (req, res) => {
   try {
     const resultado = await pool.query(`
       SELECT 
+        usuarios.id,
         usuarios.nome,
         post.conteudo,
         post.criado_em
@@ -24,6 +39,29 @@ app.get("/posts", async (req, res) => {
     res.json(resultado.rows);
   } catch (erro) {
     res.status(500).json({ erro: "Erro ao buscar postagens" });
+  }
+});
+
+// CREATE POST
+app.post("/post", async (req, res) => {
+  try {
+    const { titulo, conteudo, usuario_id } = req.body;
+    const resultado = await pool.query(
+      `
+      INSERT INTO post (titulo, conteudo, usuario_id)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
+      [titulo, conteudo, usuario_id],
+    );
+    res.status(201).json({
+      mensagem: "Post criado com sucesso",
+      post: resultado.rows[0],
+    });
+  } catch (erro) {
+    res.status(500).json({
+      erro: "Erro ao criar postagem",
+    });
   }
 });
 
