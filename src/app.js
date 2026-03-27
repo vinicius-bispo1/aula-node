263754af8442a56fbf30155f09ef237de3b4360f
vinicius-bispo1 <vini.bispo125@gmail.com>
2026-03-26 23:30:44 -0300

    codigo-aula2-27/03

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const pool = require("./config/db");
 const app = express();
 app.use(express.json());
 
+// colocar no horario local
+function formatarData(data) {
+  return new Date(data).toLocaleString("pt-BR", {
+    timeZone: "America/Bahia",
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("<h1>Rede Social!</h1>");
 });
@@ -33,11 +40,18 @@ app.get("/posts", async (req, res) => {
         post.criado_em
       FROM post
       JOIN usuarios 
-      ON post.usuario_id = usuarios.id
+        ON post.usuario_id = usuarios.id
       ORDER BY post.criado_em DESC
     `);
-    res.json(resultado.rows);
+
+    const dados = resultado.rows.map((post) => ({
+      ...post,
+      criado_em: formatarData(post.criado_em),
+    }));
+
+    res.json(dados);
   } catch (erro) {
+    console.error(erro);
     res.status(500).json({ erro: "Erro ao buscar postagens" });
   }
 });
@@ -46,6 +60,7 @@ app.get("/posts", async (req, res) => {
 app.post("/post", async (req, res) => {
   try {
     const { titulo, conteudo, usuario_id } = req.body;
+
     const resultado = await pool.query(
       `
       INSERT INTO post (titulo, conteudo, usuario_id)
@@ -54,15 +69,43 @@ app.post("/post", async (req, res) => {
       `,
       [titulo, conteudo, usuario_id],
     );
+
+    const post = {
+      ...resultado.rows[0],
+      criado_em: formatarData(resultado.rows[0].criado_em),
+    };
+
     res.status(201).json({
       mensagem: "Post criado com sucesso",
-      post: resultado.rows[0],
+      post,
     });
   } catch (erro) {
     res.status(500).json({
       erro: "Erro ao criar postagem",
     });
   }
+});
+
+// UPDATE POST
+app.put("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const { titulo, conteudo } = req.body;
+
+  const resultado = await pool.query(
+    "UPDATE post SET titulo=$1, conteudo=$2 WHERE id=$3 RETURNING *",
+    [titulo, conteudo, id],
+  );
+
+  res.json(resultado.rows[0]);
+});
+
+// DELETE POST
+app.delete("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await pool.query("DELETE FROM post WHERE id=$1", [id]);
+
+  res.json({ message: "Post deletado" });
 });
 
 module.exports = app;
